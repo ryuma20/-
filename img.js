b79d4d4e9d451ab780b6c3d2a67b6031d4007027
ryuma20 <76700837+ryuma20@users.noreply.github.com>
2021-01-22 21:36:52 +0900

    Update img.js

--- img.js
+++ img.js
@@ -1,5 +1,5 @@
 var img;
 function setup(){
 createCanvas(1000,1000);
-img=load('https://github.com/ryuma20/memeta/blob/main/E2EFB3C8-5030-485D-815F-A25C65D6376F.jpeg');
+img=load('E2EFB3C8-5030-485D-815F-A25C65D6376F.jpeg');
 }
