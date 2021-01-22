62b6f65cb3ab413c16f5ca08e28d0f0c1d57c218
ryuma20 <76700837+ryuma20@users.noreply.github.com>
2021-01-23 02:11:39 +0900

    Update img.js

--- img.js
+++ img.js
@@ -1,7 +1,7 @@
 var img;
 function setup(){
 createCanvas(1000,1000);
-img=load('E2EFB3C8-5030-485D-815F-A25C65D6376F.jpeg');
+img=load('https://github.com/ryuma20/memeta/blob/main/E2EFB3C8-5030-485D-815F-A25C65D6376F.jpeg');
 }
 function draw(){
   image(img,0,0);
